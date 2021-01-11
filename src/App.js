df2e3051a5ddc22530e30e187872b22c2a493ba7
Iuliia Chistiakova <julia.hakimyanova@gmail.com>
2021-01-11 18:58:25 -0500

    Added search input

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
     super();
     this.state = {
       monsters: [],
+      searchField: "",
     };
   }
 
@@ -20,6 +21,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <input
+          type="search"
+          placeholder="search monsters"
+          onChange={(e) => {
+            this.setState({ searchField: e.target.value }, () =>
+              console.log(this.state)
+            );
+          }}
+        />
         <CardList>
           {this.state.monsters.map((monster) => (
             <Card key={monster.id} monster={monster} />
